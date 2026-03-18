396a9a604b3ca5b9187cbbcac1c1c0a5f49fc6c1
Adsutori <adsutori@outlook.com>
2026-03-18 11:30:44 +0100

    fix scroll bar bug

--- PocketUp/Dashboard/urls.py
+++ PocketUp/Dashboard/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('transactions/', views.transactions, name='transactions'),
-    path('budget/', views.transactions, name='budget'),
-    path('goals/', views.transactions, name='goals'),
-    path('reports/', views.transactions, name='reports'),
-    path('settings/', views.transactions, name='settings'),
+    path('budget/', views.budget, name='budget'),
+    path('goals/', views.goals, name='goals'),
+    path('reports/', views.reports, name='reports'),
+    path('settings/', views.settings, name='settings'),
 ]
